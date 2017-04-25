041b0c8d5491494f5b751ac923973776788dd627
caolanf <caolanfleming9876@gmail.com>
2017-04-25 18:07:41 +0100

    Create longest increasing subsequence.py

---
+++ Dynamic Programming/longest increasing subsequence.py
@@ -0,0 +1,9 @@
+array = map(int, input().split())
+
+dp = [1]*len(array)
+for i in range(len(dp)):
+    for x in range(i):
+        if array[i] >= array[x]:
+            dp[i] = max(dp[x]+1,dp[i])
+            
+print(max(dp))
