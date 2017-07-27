c396a1dbc91f168169b8ad8d1933a398acee68f5
caolanf <caolanfleming9876@gmail.com>
2017-07-27 17:30:32 +0100

    Update Segment Tree.cpp
    added update

--- Graphs/Segment Tree.cpp
+++ Graphs/Segment Tree.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 class SegmentTree {
-  	private: 
+	private: 
 	vector<int> st, A;
 	int n;
 	int left (int p) { return p << 1; }
@@ -20,6 +20,19 @@ class SegmentTree {
 		}
 	}
 	
+	void update (int p, int L, int R, int i, int new_val) {
+		if (!(i > R || i < L)) {
+			if (L == R)
+				st[p] = L;
+			else {
+				update (left(p), L, (L+R)/2, i, new_val);
+				update (left(p), (L+R)/2+1, R, i, new_val);
+				int p1 = st[left(p)], p2 = st[right(p)];
+				st[p] = (A[p1] <= A[p2]) ? p1 : p2;
+			}
+		}
+	}
+	
 	int rmq (int p, int L, int R, int i, int j) {
 		if (i > R || j < L ) return -1;     // Out of scope
 		if (L >= i && R <= j) return st[p]; // Inside query range
@@ -32,6 +45,7 @@ class SegmentTree {
 		
 		return (A[p1] <= A[p2]) ? p1 : p2;
 	}
+	
 	public:
 	SegmentTree (const vector<int> &_A) {
 		A = _A;
@@ -41,6 +55,7 @@ class SegmentTree {
 	}
 	
 	int rmq (int i, int j) { return rmq(1, 0, n-1, i, j); }
+	void update (int i, int new_val) { A[i] = new_val; update(1, 0, n-1, i, new_val); }
 };
 
 int main()
@@ -48,5 +63,7 @@ int main()
 	vector<int> numbers = {5,3,7,2,1,9};
 	SegmentTree st(numbers);
 	
-	cout << "RMQ(1,5) = " << st.rmq(1,5);
+	cout << "RMQ(1,5) : " << st.rmq(1,5) << endl;
+	st.update(2,0);
+	cout << "New min : " << st.rmq(0,5) << endl;
 }
